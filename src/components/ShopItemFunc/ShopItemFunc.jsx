67710c392e3f5956a82e90480d88d838fab346b7
Fox Say <Lisick1999@yandex.ru>
2024-05-31 17:35:02 +0300

    added props

--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,31 +1,19 @@
 import classes from './shopItemFunc.module.css';
 
-export const ShopItemFunc = () => {
-    const item = {
-        brand: 'Tiger of Sweden',
-        title: 'Leonard coat',
-        description: 'Minimalistic coat in cotton-blend',
-        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-        price: 399,
-        currency: '£',
-    };
-  return <div className={classes["main-content"]}>
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
+export const ShopItemFunc = (props) => {
+    const {brand, title, description, descriptionFull, price, currency} = props.item;
+    return <div className={classes["main-content"]}>
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
         <div className={classes["description"]}>
-            {item.descriptionFull}
+            {descriptionFull}
         </div>
         <div className={classes["highlight-window mobile"]}><div className={classes["highlight-overlay"]}></div></div>
         <div className={classes["divider"]}></div>
         <div className={classes["purchase-info"]}>
-            <div className={classes["price"]}>{`${item.currency} ${item.price}`}</div>
+            <div className={classes["price"]}>{`${currency}${price}`}</div>
             <button>Добавить в корзину</button>
         </div>
     </div>
-//   <article>
-//     <h2 className={classes["name"]}>{user.name}</h2>
-//     <img src={user.avatar} alt={`${user.name} avatar`} />
-//     <p>{user.status}</p>
-//   </article>;
 };
